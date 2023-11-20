04ed78fb6ccbf9b2e4fc9992223858fde869bfde
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 22:51:36 -0400

    Objects

---
+++ app/src/main/java/com/example/kotlin_poo/KotlinPoo.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_poo
+
+fun main() {
+
+    // Create a c1 object of the Car Class
+    var c1 = Car()
+
+    // Access the properties
+    c1.brand = "BMW"
+    println(c1.brand)
+
+    // Access the Functions (Methods)
+    c1.drive()
+    c1.brake()
+
+}
