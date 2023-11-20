1b0bd9b92c3b3e267de93ce8d2e89c1ee7442f20
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 22:44:36 -0400

    Classes

---
+++ app/src/main/java/com/example/kotlin_poo/Car.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_poo
+
+class Car {
+
+    // Attributes - Property
+    // variables that belongs to the class
+    var brand = "Ford"
+    var year = 2023
+    var color = "Red"
+
+    // Functions - behaviours
+    fun drive() {
+        println("Driving...")
+    }
+
+    fun brake() {
+        println("Braking...")
+    }
+
+}
